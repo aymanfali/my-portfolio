99bca38632568779d941f89633f7081c5138bc8f
Ayman F. Ali <ayman4swd@gmail.com>
2025-07-14 15:11:19 +0300

    feat: using only css to change between dark and light modes

--- js/app.js
+++ js/app.js
@@ -7,27 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const mainNavToggle = document.querySelector(".main-nav-toggle");
 
-  const themeToggle = document.getElementById("theme-toggle");
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-  // Check for saved theme preference or use system preference
-  const currentTheme = localStorage.getItem("theme");
-  if (currentTheme === "dark" || (!currentTheme && prefersDarkScheme.matches)) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    themeToggle.checked = true;
-  }
-
-  // Toggle theme when switch is clicked
-  themeToggle.addEventListener("change", function () {
-    if (this.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.removeAttribute("data-theme");
-      localStorage.setItem("theme", "light");
-    }
-  });
-
   mainNavToggle.addEventListener("click", function () {
     if (this.classList.contains("active-menu")) {
       this.classList.remove("active-menu");
